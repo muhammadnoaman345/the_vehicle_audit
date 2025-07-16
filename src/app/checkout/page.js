e6f99512ca4a04e391ff8b39a9fb7eff4ca7239b
Ahmed Shahid <154593144+AhmedShahid786@users.noreply.github.com>
2025-07-17 03:30:11 +0500

    decrease price for testing

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -428,7 +428,7 @@ function StripeWrapper() {
   const searchParams = useSearchParams();
   const packageName = searchParams.get("package");
   const name = packageName.split("-")[1];
-  const amount = name === "silver" ? 49.99 : name === "gold" ? 84.99 : 109.99;
+  const amount = name === "silver" ? 1 : name === "gold" ? 2 : 3
 
   useEffect(() => {
     fetch("/api/create-payment-intent", {
