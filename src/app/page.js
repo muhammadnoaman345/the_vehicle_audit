148c5d1df75abf6056f447fdc92f00615c2cb861
Ahmed Shahid <a.razashahid19@gmail.com>
2025-07-17 02:25:45 +0500

    initialize firebase in home page

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,19 @@ import OurServices from "@/components/OurServices/OurServices";
 import { Pricing } from "@/components/Pricing/Pricing";
 import { Testimonials } from "@/components/Testimonials/Testimonials";
 import WhyChooseUs from "@/components/WhyChooseUs/WhyChooseUs";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MSID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MID,
+};
+
+export const app = initializeApp(firebaseConfig);
 
 function Home() {
   return (
