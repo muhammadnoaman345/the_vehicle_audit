884bb61c52bbc3e244b2ccc928922d36a7767173
Ahmed Shahid <a.razashahid19@gmail.com>
2025-07-18 00:18:05 +0500

    add whatsapp icon

---
+++ src/components/WhatsappIcon/WhatsappIcon.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { images } from "../../../public/assets/images";
+
+export default function WhatsappIcon() {
+  return (
+    <Link
+      href="http://wa.me/+447577303327"
+      className="absolute fixed z-50 right-4 bottom-4"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={images.socials.wa}
+        className="h-12 xl:h-16 w-12 xl:w-16 rounded-full"
+      />
+    </Link>
+  );
+}
