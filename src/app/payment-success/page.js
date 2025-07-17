96cd30801483f37deb1f32b0c854b5d8c55044d6
Ahmed Shahid <a.razashahid19@gmail.com>
2025-07-18 03:40:24 +0500

    update payment success page

--- src/app/payment-success/page.js
+++ src/app/payment-success/page.js
@@ -1,29 +1,6 @@
-"use client";
-
-import Loading from "@/components/Loading/Loading";
-import { redirect, useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
-import { doc, getFirestore, updateDoc } from "firebase/firestore";
 import { CheckCircle } from "lucide-react";
-import { app } from "../page";
 
-function Page() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("doc-id");
-  const db = getFirestore();
-
-  useEffect(() => {
-    async function updatePaymentStatus() {
-      if (id) {
-        await updateDoc(doc(db, "sales", id), {
-          paymentStatus: "paid",
-        });
-        redirect("/");
-      }
-    }
-    updatePaymentStatus();
-  }, [id]);
-
+export default function Page() {
   return (
     <div className="w-screen h-[50dvh] flex items-center justify-center !text-white">
       <div className="bg-primary rounded-2xl shadow-xl p-8 flex flex-col items-center">
@@ -38,13 +15,5 @@ function Page() {
         </p>
       </div>
     </div>
-  );
-}
-
-export default function PageWithSuspense() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <Page />
-    </Suspense>
   );
 }
