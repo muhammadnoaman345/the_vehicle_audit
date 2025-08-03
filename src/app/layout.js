527731ff58d084dd09f9dd84bc77bc4819f3d789
Ahmed Shahid <154593144+AhmedShahid786@users.noreply.github.com>
2025-08-03 23:53:15 +0500

    add tawk.to script

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main className="z-10">{children}</main>
         <Footer />
-        <WhatsappIcon />
+        <script type="text/javascript"> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0]; s1.async=true; s1.src='https://embed.tawk.to/688d293ecabd591931918f0e/1j1jmi73c'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script>
       </body>
     </html>
   );
