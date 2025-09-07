ee2286597eef17566a6d50f0aebdeda4a5f9043f
Muhammad Noaman <muhammadnoaman345@gmail.com>
2025-09-08 01:01:57 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+// app/layout.js
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import "./globals.css";
@@ -20,21 +21,25 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
 
-        {/* ✅ Tawk.to Live Chat */}
-        <Script id="tawk-to" strategy="afterInteractive">
-          {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),
-                  s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/688d293ecabd591931918f0e/1j1jmi73c';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
+        {/* ✅ Tawk.to Script */}
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),
+                    s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/688d293ecabd591931918f0e/1j1jmi73c';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
